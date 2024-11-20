26bf731362e0a624d9b83a8783d3a8fbe09ce9fd
TLinet33 <thomas.linet@etu.estia.fr>
2024-11-20 11:11:49 +0100

    Modif php

---
+++ php.php
@@ -0,0 +1,22 @@
+<?php
+echo "Bonjour le monde !";
+
+// ... encore du code
+
+echo "Dernière instruction";
+
+// le script se termine ici, sans la balise de fermeture PHP
+locale_accept_from_http
+echo
+<a href="Il était le divien enfant qui avait mal aux chicos"></a>
+
+echo "Bonjour les  monde !";
+
+// ... encore du code
+
+echo "Dernière modifiaction";
+
+// le script se termine ici, sans la balise de fermeture PHP
+locale_accept_from_http
+echo
+<a href="Il était le divien enfant qui avait mal aux chicos"></a>
